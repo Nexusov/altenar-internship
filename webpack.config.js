6117bb6aecb5c068db65c32bf907db78a1116e08
Nexus <egor2001grashin@gmail.com>
2023-12-09 06:10:46 +0300

    made more alias for webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,16 @@ module.exports = {
 	resolve: {
 		extensions: ['.tsx', '.ts', '.js'],
 		alias: {
-      styles: path.resolve(__dirname, 'src/styles/')
+      styles: path.resolve(__dirname, 'src/styles/'),
+			pages: path.resolve(__dirname, 'src/pages/'),
+			components: path.resolve(__dirname, 'src/components/'),
+			services: path.resolve(__dirname, 'src/services/'),
+			assets: path.resolve(__dirname, 'src/assets/'),
+      utils: path.resolve(__dirname, 'src/utils/'),
+      hooks: path.resolve(__dirname, 'src/hooks/'),
+      constants: path.resolve(__dirname, 'src/constants/'),
+      contexts: path.resolve(__dirname, 'src/contexts/'),
+      routes: path.resolve(__dirname, 'src/routes/'),
     },
 	},
 	plugins: [
