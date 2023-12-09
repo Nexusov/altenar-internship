953d2c2cb97b4b93dfd7e0a843d4c9b317942506
Nexus <egor2001grashin@gmail.com>
2023-12-09 06:11:18 +0300

    started Index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Index from "pages/index/Index"
+import Creation from "pages/creation/Creation"
+
+/* TODO:  REMOVE ALL THE CLASSNAMES*/
+
 const App = () => {
   return (
     <>
-      <p>Hello!</p>
+      <Index />
     </>
   )
 }
